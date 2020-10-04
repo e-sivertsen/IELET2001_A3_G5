41ee3757872f6aa76edec1eed9d2e5c514da342c
e-sivertsen <e.sivertsen@hotmail.com>
2020-10-04 13:26:25 +0200

    Init

--- Warmup-Python/simple_tcp_client.py
+++ Warmup-Python/simple_tcp_client.py
@@ -1,63 +1,49 @@
-# A Simple TCP client, used as a warm-up exercise for socket programming assignment.
-# Course IELEx2001, NTNU
-
 import random
 import time
 from socket import *
 
-# Hostname of the server and TCP port number to use
 HOST = "datakomm.work"
 PORT = 1301
 
-# The socket object (connection to the server and data excahgne will happen using this variable)
 client_socket = None
 
 
 def connect_to_server(host, port):
-    """
-    Try to establish TCP connection to the server (the three-way handshake).
-    :param host: The remote host to connect to. Can be domain (localhost, ntnu.no, etc), or IP address
-    :param port: TCP port to use
-    :return: True when connection established, false otherwise
-    """
-    # The "global" keyword is needed so that this function refers to the globally defined client_socket variable
     global client_socket
 
     client_socket = socket(AF_INET, SOCK_STREAM)
 
-    # TODO - implement this method
-    # Remember to catch all possible exceptions the socket can throw in case you have not worked with exceptions,
-    # the syntax is as follows:
-    # try:
-    #     do_what_you_want_to_do_in_code()
-    # except IOError as e:
-    #     print("Error happened:", e)
-    #     what_to_do_in_case_of_error()
-    return False
+    try:
+        client_socket.connect((host, port))
+        return True
+    
+    except IOError as e:
+        print("Error: ", e)
+        return False
 
 
 def close_connection():
-    """
-    Close the TCP connection to the remote server.
-    :return: True on success, false otherwise
-    """
-    # The "global" keyword is needed so that this function refers to the globally defined client_socket variable
     global client_socket
 
-    # TODO - implement this method
-    return False
+    try:
+        client_socket.close()
+        return True
+
+    except IOError as e:
+        Print("Error: ", e)
+        return False
 
 
 def send_request_to_server(request):
-    """
-    :param request: The request message to send. Do NOT include the newline in the message!
-    :return: True when message successfully sent, false on error.
-    """
-    # The "global" keyword is needed so that this function refers to the globally defined client_socket variable
     global client_socket
 
-    # TODO - implement this method
-    return False
+    try:
+        client_socket.send(request.encode())
+        return True
+
+    except IOError as e:
+        Print("Error: ", e)
+        return False
 
 
 def read_response_from_server():
